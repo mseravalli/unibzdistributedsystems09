7fbf542fd81012069203067dfc3380fc6cc4fae3
marco.seravalli <marco.seravalli@8390ecc0-c868-11de-a2e8-8d36a3108c74>
2009-11-30 14:02:54 +0000

    il nickname viene attaccato sul server e non sul client

--- chat/src/server/ServerConnection.java
+++ chat/src/server/ServerConnection.java
@@ -53,6 +53,11 @@ public class ServerConnection extends Thread {
 		    
 				String data = in.readUTF();
 				System.out.println(data);
+				/*
+				 * adds at the beginning of the message the nickname of the sender
+				 */
+				int index = this.clients.indexOf(clientSocket);
+				data = this.nicknames.get(index) + data;
 
 				/*
 				 * the message is sent so each client present in the clients list
